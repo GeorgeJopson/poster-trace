041a9bd57d544b5bdb6947ff9dc8cd4de309861d
George Jopson <georgejopson@gmail.com>
2026-08-12 12:03:32 +0100

    Add initial styling to sketch out the navbar and header, and move global styles to global.css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,20 @@
-import GlobalStyles from "@/components/GlobalStyles";
+import "./global.css";
+import {DM_Sans} from "next/font/google";
 
 export const metadata = {
   title: "PosterTrace",
 };
 
+const dm_sans = DM_Sans({
+  subsets: ['latin'],
+  variable: '--font-dm-sans',
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${dm_sans.variable}`}>
       <head>
         <title>PosterTrace</title>
-        <GlobalStyles/>
       </head>
       <body>{children}</body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,56 @@
 import styled from "styled-components";
 
+import { Aperture } from 'feather-icons-react';
+
 export default function Home() {
   return (
     <main>
-      <Title>Title</Title>
+      <NavBar>
+        <Aperture size={28} strokeWidth={"1.5px"}/>
+        <p>PosterTrace</p>
+      </NavBar>
+      <HeroWrapper>
+      </HeroWrapper>
+
+      <Spacer/>
     </main>
   );
 }
 
-const Title = styled.h1`
-  font-size: 4rem;
+const Spacer = styled.div`
+  height: 300vh;
+`
+
+const HeroWrapper = styled.div`
+  height: 100px;
+  border-radius: 0 0 var(--spacing-10) var(--spacing-10);
+
+  background-image: linear-gradient(
+          135deg,
+          var(--color-orange-300) 0%,
+          var(--color-orange-500) 100%
+  );
+  padding: 0 64px;
+`
+const NavBar = styled.div`
+  border-radius: 8px;
+  padding: 0 4px;
+  gap: 8px;
+  position: fixed;
+  top: 16px;
+  left: 64px;
+  right: 64px;
+  font-size: calc(32 / 16 * 1rem);
+  font-weight:900;
+  font-family: var(--font-dm-sans),sans-serif;
+  
+  background-image: linear-gradient(
+          to left,
+          var(--color-orange-300-transparent) 0%,
+          var(--color-orange-200-transparent) 100%
+  );
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
 `
