37ac39c11b0178ef5bcf2580a93b3a8dcf697e33
George Jopson <georgejopson@gmail.com>
2026-08-13 12:21:33 +0100

    Add initial "Problem" section

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,34 @@ import styled from "styled-components";
 
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
+import Header from "@/components/Header";
+import CentralColumn from "@/components/CentralColumn";
 
 export default function Home() {
   return (
     <main>
       <NavBar/>
-      <Hero/>
+      <ContentGroup>
+        <Hero/>
+        <ProblemSection>
+          <CentralColumn>
+            <Header>The Problem</Header>
+          </CentralColumn>
+        </ProblemSection>
+      </ContentGroup>
       <Spacer/>
     </main>
   );
 }
+
+const ProblemSection = styled.section`
+  padding-top: 32px;
+`
+
+const ContentGroup = styled.div`
+  padding-bottom: 64px;
+  background-color: var(--color-orange-50);
+`
 
 const Spacer = styled.div`
   height: 300vh;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import {QUERIES} from "@/constants";
+
+const Header = styled.h2`
+  font-weight: 400;
+  font-family: var(--font-inter), sans-serif;
+  text-align: center;
+  
+  font-size: ${64/16}rem;
+  @media ${QUERIES.tabletAndDown} {
+    font-size: ${48/16}rem;
+  }
+  @media ${QUERIES.phoneAndDown} {
+    font-size: ${32/16}rem;
+  }
+`
+export default Header;
