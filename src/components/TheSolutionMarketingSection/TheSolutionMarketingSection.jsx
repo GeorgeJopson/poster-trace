c7c2a1d613e9914d99b64adbc8cb9efb4e55511a
George Jopson <georgejopson@gmail.com>
2026-08-14 12:06:30 +0100

    Add poster images to TheSolutionMarketingSection.jsx

--- src/components/TheSolutionMarketingSection/TheSolutionMarketingSection.jsx
+++ src/components/TheSolutionMarketingSection/TheSolutionMarketingSection.jsx
@@ -4,6 +4,8 @@ import Header from "@/components/Header";
 import CentralColumn from "@/components/CentralColumn";
 import React from "react";
 import List from "@/components/TheSolutionMarketingSection/List";
+import Image from "next/image";
+import {QUERIES} from "@/constants";
 
 export default function TheSolutionMarketingSection() {
   return (
@@ -24,12 +26,110 @@ export default function TheSolutionMarketingSection() {
               </>
             </List>
           </Column>
-          <Column></Column>
+          <ImageColumn>
+            <ImageSeven>
+              <Image loading="lazy" src={"/poster-images/poster-image-7.jpg"} width={264} height={198} alt={"Picture of Posters"}/>
+              <DarkTint/>
+            </ImageSeven>
+            <ImageSix>
+              <Image loading="lazy" src={"/poster-images/poster-image-6.jpg"} width={264} height={198} alt={"Picture of Posters"}/>
+              <DarkTint/>
+            </ImageSix>
+            <ImageFive>
+              <Image loading="lazy" src={"/poster-images/poster-image-4.jpg"} width={161} height={240} alt={"Picture of Posters"}/>
+              <DarkTint/>
+            </ImageFive>
+            <ImageFour>
+              <Image loading="lazy" src={"/poster-images/poster-image-3.jpg"} width={275} height={183} alt={"Picture of Posters"}/>
+              <DarkTint/>
+            </ImageFour>
+            <ImageThree>
+              <Image loading="lazy" src={"/poster-images/poster-image-5.jpg"} width={205} height={270} alt={"Picture of Posters"}/>
+              <DarkTint/>
+            </ImageThree>
+            <ImageTwo>
+              <Image loading="lazy" src={"/poster-images/poster-image-2.jpg"} width={264} height={198} alt={"Picture of Posters"}/>
+              <DarkTint/>
+            </ImageTwo>
+            <ImageOne>
+              <Image loading="lazy" src={"/poster-images/poster-image-1.jpg"} width={285} height={203} alt={"Picture of Posters"}/>
+              <Tint/>
+            </ImageOne>
+          </ImageColumn>
         </ColumnLayoutWrapper>
       </CentralColumn>
     </ProblemSection>
   );
 }
+
+const ImageWrapper = styled.div`
+  overflow:clip;
+  position: absolute;
+  border-radius: 8px;
+`
+
+const ImageOne = styled(ImageWrapper)`
+  top:50%;
+  left: 50%;
+  transform: translate(-50%, -50%) rotate(4deg);
+  width: 285px;
+  @media ${QUERIES.phoneAndDown} {
+    width: 275px;
+  }
+`
+
+const ImageForDesktop = styled(ImageWrapper)`
+  @media ${QUERIES.tabletAndDown} {
+    display: none;
+  }
+`
+
+const ImageTwo = styled(ImageForDesktop)`
+  top:10%;
+  left:10%;
+  transform: rotate(-4deg);
+`
+
+const ImageThree = styled(ImageForDesktop)`
+  top:5%;
+  right:4%;
+  transform: rotate(-8deg);
+`
+const ImageFour = styled(ImageForDesktop)`
+  bottom:5%;
+  left:6%;
+  transform: rotate(8deg);
+`
+const ImageFive = styled(ImageForDesktop)`
+  bottom: 1%;
+  right: 5%;
+  transform: rotate(20deg);
+`
+const ImageSix = styled(ImageForDesktop)`
+  top:1%;
+  left: 50%;
+  transform: translateX(-50%) rotate(4deg);
+`
+
+const ImageSeven = styled(ImageForDesktop)`
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%) rotate(2deg);
+`
+
+const Tint = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-orange-50);
+  opacity:0.1;
+  position: absolute;
+  top:0;
+  left:0;
+`
+
+const DarkTint = styled(Tint)`
+  background-color: var(--color-orange-600);
+`
 
 
 const ProblemSection = styled.section`
@@ -41,13 +141,20 @@ const ColumnLayoutWrapper = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  row-gap: 16px;
 `
 
 const Column = styled.div`
   flex: 1;
-  display: flex;
-  flex-direction: column;
-  min-width: ${256 / 16}rem;
+  min-width: ${340 / 16}rem;
+  position: relative;
+  @media ${QUERIES.phoneAndDown} {
+    min-width: ${288 / 16}rem;
+  }
+`
+
+const ImageColumn = styled(Column)`
+  min-height: 220px;
 `
 
 const SolutionHeader = styled(Header)`
