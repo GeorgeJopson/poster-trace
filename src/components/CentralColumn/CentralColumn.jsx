f4b5928e543067f4d5f373bcf34531e6e16d3cab
George Jopson <georgejopson@gmail.com>
2026-08-26 18:36:37 +0100

    Add poster photos to header section

--- src/components/CentralColumn/CentralColumn.jsx
+++ src/components/CentralColumn/CentralColumn.jsx
@@ -30,4 +30,5 @@ const Spacer = styled.div`
 
 const CentralDiv = styled.div`
   flex:1;
+  position: relative;
 `
--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -5,11 +5,42 @@ import CentralColumn from "@/components/CentralColumn";
 import Title from "@/components/Section1/Title";
 import ButtonGroup from "@/components/Section1/ButtonGroup";
 import {QUERIES} from "@/constants";
+import Image from "next/image";
+
+const posterSize = 256;
+const root2 = 1.414213562;
 
 function Section1() {
   return (
     <Wrapper>
       <CentralColumn>
+        <PosterThree
+          width={posterSize}
+          height={posterSize * root2}
+          src={"/poster-images/stock-poster-3.png"}
+          alt={
+            "Poster Advertising Upcoming Events like a Grand Opening and Creative Gathering. " +
+            "There is a QR code in the bottom right hand corner."}
+        />
+
+        <PosterTwo
+          width={posterSize}
+          height={posterSize * root2}
+          src={"/poster-images/stock-poster-2.png"}
+          alt={
+            "Poster Advertising Upcoming Events like a Grand Opening and Creative Gathering. " +
+            "There is a QR code in the bottom right hand corner."}
+        />
+
+        <PosterOne
+          width={posterSize}
+          height={posterSize * root2}
+          src={"/poster-images/stock-poster-1.png"}
+          alt={
+            "Poster Advertising Upcoming Events like a Grand Opening and Creative Gathering. " +
+            "There is a QR code in the bottom right hand corner."}
+        />
+
         <Title>
           Poster&nbsp;Analytics,<br/>
           For&nbsp;Communities,<br/>
@@ -27,6 +58,31 @@ function Section1() {
 }
 
 export default Section1;
+
+const PosterImage = styled(Image)`
+  position: absolute;
+  width: auto;
+  height: auto;
+`
+
+const PosterOne = styled(PosterImage)`
+  top: 0;
+  right: 10%;
+  
+  rotate: 4deg;
+`
+
+const PosterTwo = styled(PosterImage)`
+  top: 30%;
+  right: 5%;
+  rotate: 8deg;
+`
+
+const PosterThree = styled(PosterImage)`
+  top: 15%;
+  right: 20%;
+  rotate: -6deg;
+`
 
 const Wrapper = styled(ContentGroup)`
   padding-top: calc(48px);
