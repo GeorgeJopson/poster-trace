e920eb6e814c485aae9b0503c580b7195453a98c
George Jopson <georgejopson@gmail.com>
2026-08-26 16:44:45 +0100

    Introduce new small phone breakpoint

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,12 @@
 export const BREAKPOINTS = {
+  smallPhoneMax: 400,
   phoneMax: 600,
   tabletMax: 950,
   laptopMax: 1300,
 }
 
 export const QUERIES = {
+  'smallPhoneAndDown': `(max-width: ${BREAKPOINTS.smallPhoneMax / 16}rem)`,
   'phoneAndDown': `(max-width: ${BREAKPOINTS.phoneMax / 16}rem)`,
   'tabletAndDown': `(max-width: ${BREAKPOINTS.tabletMax / 16}rem)`,
   'laptopAndDown': `(max-width: ${BREAKPOINTS.laptopMax / 16}rem)`,
