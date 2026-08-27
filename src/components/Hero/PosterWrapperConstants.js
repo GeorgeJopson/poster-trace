aa1672efe136679d69cc8dff1635771ecff50c97
George Jopson <georgejopson@gmail.com>
2026-08-27 15:46:42 +0100

    Make posters increase in size and go to front on hover

--- src/components/Hero/PosterWrapperConstants.js
+++ src/components/Hero/PosterWrapperConstants.js
@@ -5,4 +5,9 @@ export const posterWrapperDefaults = `
   width: calc(${posterSize}px * var(--scale-factor));
   height: calc(${posterSize * root2}px * var(--scale-factor));
   position: relative;
+  &:hover {
+    z-index: 1;
+    scale: 1.01;
+  }
+  transition: scale 0.1s;
 `;
