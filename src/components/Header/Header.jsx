b58632f6ccf45351cf2fe0fbf4d39a53dbf50f8d
George Jopson <georgejopson@gmail.com>
2026-09-07 11:42:27 +0100

    Update Headers to have spaces between lines

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import styles from "./Header.module.css";
+import {crypto} from "next/dist/compiled/@edge-runtime/primitives";
 
 export default function Header({children, type, className}){
   let output = [];
+  console.log(children)
   for (const line of children){
     if(typeof line === "string"){
       const words = line.split(" ");
+
+      let outputLine = []
       for (const word of words){
-        output.push(
-          <div className={styles.word} key={word}>
+        outputLine.push(
+          <div className={styles.word} key={crypto.randomUUID()}>
             {Array.from(word).map(
               (character,index)=><span className={styles.character} key={index}>{character}</span>)
               }
           </div>
         )
-        output.push(<span key={crypto.randomUUID()}>&nbsp;</span>);
+        outputLine.push(<span key={crypto.randomUUID()}>&nbsp;</span>);
       }
-      output.pop();
-    }else{
-      output.push(line);
+      outputLine.pop();
+
+      output.push(<div className={styles.line} key={crypto.randomUUID()}>{outputLine}</div>)
     }
   }
 
