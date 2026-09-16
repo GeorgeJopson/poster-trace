41b8144d88953c4a089cc55b7871d4fddbb41a67
George Jopson <georgejopson@gmail.com>
2026-09-16 21:36:04 +0100

    Stop displaying session info in dashboard/page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,11 @@
-import { auth } from "@/lib/auth";
-
-import { headers } from "next/headers";
 import CentralColumn from "@/components/CentralColumn";
 
 export default async function DashboardPage() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
 
   return (
     <div>
         <CentralColumn>
-            <p>{JSON.stringify(session)}</p>
+            <p>Welcome to the Dashboard!!!</p>
         </CentralColumn>
     </div>
   );
